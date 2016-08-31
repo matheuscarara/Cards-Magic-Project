bf97182f229fd79728c6926e37f220c85a65ea0a
matheuscarara <matheusmcarara@gmail.com>
2016-08-30 21:07:54 -0300

    Cards&Magic Atualizado Fizemos todas classes, implementamos a lógica inicial da iteração 1. falta fazer interface, mensagens das exceções e fazer as cartas.

---
+++ src/modelo/ExcecaoCartaNaoExiste.java
@@ -0,0 +1,9 @@
+package modelo;
+
+public class ExcecaoCartaNaoExiste extends Exception {
+	
+	public ExcecaoCartaNaoExiste () {
+		super();
+	}
+
+}
---
+++ src/modelo/RepositorioJogadores.java
@@ -0,0 +1,47 @@
+package modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RepositorioJogadores {
+	
+	private static List<Jogador> jogadores = new ArrayList<Jogador>();
+	
+	public static void criaJogador(String login, String senha) throws ExcecaoJogadorJaExiste{
+		if (retornaIndiceJogador(login) != -1) {
+			throw new ExcecaoJogadorJaExiste();
+		} else {
+			Jogador jogador = new Jogador(login, senha, jogadores.size());
+			jogadores.add(jogador);
+		}
+	}
+
+	private static Integer retornaIndiceJogador(String login) {
+		int i;
+		for (i = 0; i < jogadores.size(); i++) {
+			if (jogadores.get(i).getLogin().equals(login)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public static Jogador entrar (String login, String senha) throws ExcecaoJogadorNaoExiste, ExcecaoSenhaErrada{
+		int indice = retornaIndiceJogador(login);
+		if (indice == -1)
+			throw new ExcecaoJogadorNaoExiste();
+		else 
+			if (jogadores.get(indice).verifica(senha))
+				return jogadores.get(indice);
+			else
+				throw new ExcecaoSenhaErrada();
+	}
+	
+	public static Jogador getJogador (Integer index) throws ExcecaoJogadorNaoExiste {
+		if (index >= jogadores.size() || index < 0) {
+			throw new ExcecaoJogadorNaoExiste();
+		}
+		return jogadores.get(index);
+	}
+
+}
